29d6794baae7e1b7cdfc25838f1b131a52a00b1a
Lucas Barreto <93289129+LucasB712@users.noreply.github.com>
2024-03-16 22:09:25 -0300

    Update IPI.java

--- IPI.java
+++ IPI.java
@@ -1,14 +1,17 @@
+
+
 public class IPI
 {
   public void calcularIpi (float valor)
   {
+      System.out.println("Produto IPI");
     float valorTotal;
     float imposto;
-     System.out.println(valor);
+     System.out.println("O valor do produto é " + valor);
      imposto = valor * 4.6f/100;
-    System.out.println(imposto);
+    System.out.println("O valor do imposto é " + imposto);
 	valorTotal = valor + imposto;
-	System.out.println(valorTotal);
-  }
+    System.out.println("O valor total é " + valorTotal);
+}
 
 }
