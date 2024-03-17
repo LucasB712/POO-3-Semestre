e5193063c974dd79527bd96f0892e45f1afc1fb7
Lucas Barreto <93289129+LucasB712@users.noreply.github.com>
2024-03-16 22:10:28 -0300

    Update ISS.java

--- ISS.java
+++ ISS.java
@@ -1,14 +1,12 @@
 public class ISS {
-   
-   
     public void calcularIss(float valor) {
+     System.out.println("Servicos ISS");
      float imposto;
     float valorTotal;
-     System.out.println(valor);
+     System.out.println("O valor do servico é " + valor);
     imposto = valor * 4.6f/100;
-    System.out.println(imposto);
+    System.out.println("O valor do imposto é " + imposto);
     valorTotal = valor + imposto;
-    System.out.println(valorTotal);
-    }
+    System.out.println("O valor total é " + valorTotal);    }
         
     }
