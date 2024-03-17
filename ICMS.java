379c7d86c4a3bc404944ed36da9f1b88b05626a0
Lucas Barreto <93289129+LucasB712@users.noreply.github.com>
2024-03-16 22:08:26 -0300

    Update ICMS.java

--- ICMS.java
+++ ICMS.java
@@ -2,13 +2,15 @@ public class ICMS
 {
   public void calcularIcms (float valor)
   {
+      System.out.println("Produtos e Servicos ICMS");
     float valorTotal;
     float imposto;
-     System.out.println(valor);
+     System.out.println("O valor do produto/servico é " + valor);
      imposto = valor * 4.6f/100;
-    System.out.println(imposto);
+    System.out.println("O valor do imposto é " + imposto);
 	valorTotal = valor + imposto;
-	System.out.println(valorTotal);
+    System.out.println("O valor total é " + valorTotal);
   }
 
 }
+
