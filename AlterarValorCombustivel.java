22e44c370f440d3025f2522ed6773bf9f7a736c1
Lucas Barreto <93289129+LucasB712@users.noreply.github.com>
2024-03-20 11:58:55 -0300

    Create AlterarValorCombustivel.java

---
+++ AlterarValorCombustivel.java
@@ -0,0 +1,10 @@
+public class AlterarValorCombustivel{
+    public void AlterarValorCombustivel(float valor) {
+        Bomba_Combustivel combustivel = new Bomba_Combustivel();
+       combustivel.valorLitro = valor;
+        
+        System.out.println("Alterado!"");
+        System.out.println("valor");
+        
+    }
+}
